e9c2ce1ca880307947d1e005453a4fdf75cc4955
Thijs Van der Schaeghe <thijs@catlab.be>
2019-02-25 10:33:54 +0100

    Fix the routefile to not include any closures.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -45,4 +45,9 @@ class HomeController extends Controller
     {
         return view('home');
     }
+
+    public function welcome()
+    {
+        return view('welcome');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@welcome');
 
 Route::get('/docs', 'DocumentController@swagger');
 Route::get('/docs/oauth2', 'DocumentController@oauth2Redirect');
