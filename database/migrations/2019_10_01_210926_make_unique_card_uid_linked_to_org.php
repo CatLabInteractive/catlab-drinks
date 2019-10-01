8acd8934fe3f566e8327d988ee8f055ad973e6b3
Thijs Van der Schaeghe <thijs@catlab.be>
2019-10-01 23:12:14 +0200

    Allow each organisation to have their own copy of a card uid. This won't work when trying to write the cards, but at least it will not throw errors.

---
+++ database/migrations/2019_10_01_210926_make_unique_card_uid_linked_to_org.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MakeUniqueCardUidLinkedToOrg extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('cards', function (Blueprint $table) {
+
+            $table->dropUnique('cards_uid_unique');
+            $table->unique([ 'uid', 'organisation_id' ]);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('org', function (Blueprint $table) {
+            $table->dropUnique('cards_uid_organisation_id_unique');
+            $table->unique(['uid']);
+        });
+    }
+}
