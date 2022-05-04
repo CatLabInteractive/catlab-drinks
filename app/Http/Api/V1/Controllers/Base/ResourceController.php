dc666e063548dd477f67f1a79d089da70f42eb71
Thijs Van der Schaeghe <thijs@catlab.be>
2022-05-04 09:27:15 +0200

    Fix array issue in resource controller (?)

--- app/Http/Api/V1/Controllers/Base/ResourceController.php
+++ app/Http/Api/V1/Controllers/Base/ResourceController.php
@@ -202,11 +202,11 @@ class ResourceController extends Controller
         $context = new Context($action, $parameters);
 
         if ($toShow = Request::input('fields')) {
-            $context->showFields(array_map('trim', explode(',', $toShow)));
+            $context->showFields(array_map('trim', $toShow));
         }
 
         if ($toExpand = Request::input('expand')) {
-            $context->expandFields(array_map('trim', explode(',', $toExpand)));
+            $context->expandFields(array_map('trim', $toExpand));
         }
 
         $context->addProcessor(new PaginationProcessor(CursorPaginationBuilder::class));
