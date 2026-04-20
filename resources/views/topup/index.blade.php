8918efb20d3c1cabd070622b28e591dad8a1bd27
Thijs Van der Schaeghe <thijs@catlab.be>
2026-04-20 11:05:39 +0200

    Show generic page when directly accessing topup domain.

---
+++ resources/views/topup/index.blade.php
@@ -0,0 +1,11 @@
+<?php
+@extends('layouts.order')
+
+@section('content')
+	<div class="container">
+
+		<h2>CatLab Drinks: topup</h2>
+		<p>Please scan the QR code on the card to initiate a topup.</p>
+
+	</div>
+@endsection
